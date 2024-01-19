337ad9351b0979e0a0dc7148e1d95291e61e5bf6
presingi_govind_raju <presingigovindraju333@gmail.com>
2024-01-19 18:43:57 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from "react";
 
-function App() {
+
+
+
+const App =()=>{
+   const [gold,setGold] = useState(0)
+   const [silver,setSilver] = useState(0)
+   const [bronze,setBronze] = useState(0)
+
+   useEffect(()=>{
+    document.title=`Gold : ${gold} | Silver : ${silver} | bronze : ${bronze}`
+   },[gold])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Gold:{gold}</p>
+      <p>Silver:{silver}</p>
+      <p>Bronze:{bronze}</p>
+      <button onClick={()=>setGold(gold+1)}>Gold</button>
+      <button onClick={()=>setSilver(silver+1)}>Silver</button>
+      <button onClick={()=>setBronze(bronze+1)}>Bronze</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
